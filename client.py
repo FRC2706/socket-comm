1f7c64f388ddb076e46d0fa4b5a00c1711d0f906
Ken James <kjames@ciena.com>
2017-11-24 16:37:39 -0500

    Add command line setting of host, port, and message

--- client.py
+++ client.py
@@ -1,24 +1,38 @@
+#!/usr/bin/python
+
+import argparse
 import socket
 import sys
 
-HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
+received=None
+
+parser = argparse.ArgumentParser()
+parser.add_argument("message", nargs='?', help="string to send", type=str, default="Default Test Message")
+parser.add_argument("-H", "--host", help="host to connect to", type=str, default="0.0.0.0")
+parser.add_argument("-p", "--port", help="port to connect to", type=int, default=9000)
+args = parser.parse_args()
+
+port = args.port
+host = args.host
+message = args.message
 
 # Create a socket (SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    # Connect to server and send data
-    sock.connect((HOST, PORT))
-    sock.sendall(data + "\n")
+    # Connect to server and send message
+    sock.connect((host, port))
+    sock.sendall(message + "\n")
 
     # Receive data from the server and shut down
     received = sock.recv(1024)
 except Exception as e:
     print "sock communication failed %s" % str(e)
-
 finally:
     sock.close()
 
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
+if received:
+    print "To %s port %d:" % (host, port)
+    print "Sent:     {}".format(message)
+    print "Received: {}".format(received)
+
